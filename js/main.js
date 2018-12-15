24b8a64cf022ed4c649bfe324fbb517249398392
Carlson Santana Cruz <carlsonsantana@gmail.com>
2018-12-15 03:09:24 -0300

    :lipstick: Allow the user control the character

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 var app = require('app');
 var player = require('player');
+var character = require('character');
+var control = require('control');
 var canvasWidth = 720;
 var canvasHeight = 540;
+
+var character;
+var player;
 
 app.MugenJSApp.loadCharacters(
     [
@@ -30,6 +35,35 @@ app.MugenJSApp.loadCharacters(
         };
         player2.palette = player2.SFF.palette;
         player2.right = -1;
+        player = player1;
+
+        character1 = new character.Character(player);
+        character1.addDefaultEntries();
+
+        let control1 = new control.Control(player1, {
+            'ArrowUp': 'U',
+            'ArrowDown': 'D',
+            'ArrowLeft': 'B',
+            'ArrowRight': 'F',
+            'a': 'a',
+            'A': 'a',
+            's': 'b',
+            'S': 'b',
+            'd': 'c',
+            'D': 'c',
+            'q': 'x',
+            'Q': 'x',
+            'w': 'y',
+            'W': 'y',
+            'e': 'z',
+            'E': 'z'
+        });
+        document.addEventListener('keydown', function(event) {
+            control1.keyEvent(event);
+        });
+        document.addEventListener('keyup', function(event) {
+            control1.keyEvent(event);
+        });
 
         mugenjs = new app.MugenJSApp(
             player1,
